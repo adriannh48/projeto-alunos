2372e0fc69edf93a75fa550d3f92776bbf2f1f82
adriannh48 <adrianhenrique60@gmail.com>
2023-05-19 16:05:18 -0300

    remove dist

--- dist/routes/pictureRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-var _PictureController = require('../controllers/PictureController'); var _PictureController2 = _interopRequireDefault(_PictureController);
-var _login = require('../middlewares/login'); var _login2 = _interopRequireDefault(_login);
-
-const router = new (0, _express.Router)();
-
-router.post('/', _login2.default, _PictureController2.default.store);
-
-exports. default = router;
--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
-
-const port = 3001;
-_app2.default.listen(port, () => {
-  console.log(`Aplicação iniciada ${port}`);
-  console.log(`CTRL + clique em http://localhost:${port}`);
-});
